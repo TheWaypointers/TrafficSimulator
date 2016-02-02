d234afb8a0df0f64b3b3318cc8458a0bd31a5682
user <michaelszewczak@gmail.com>
2016-02-02 01:29:20 +0000

    deliberately break test to see if build on Travis picks it up

--- src/test/java/thewaypointers/trafficsimulator/tests/gui/GuiControllerTests.java
+++ src/test/java/thewaypointers/trafficsimulator/tests/gui/GuiControllerTests.java
@@ -35,6 +35,6 @@ public class GuiControllerTests {
         guiController.numberOfVehiclesChanged(15);
 
         // assert
-        Assert.assertTrue(listener.parameterChangeEvents.contains("numberOfVehicles"));
+        Assert.assertTrue(false);
     }
 }
