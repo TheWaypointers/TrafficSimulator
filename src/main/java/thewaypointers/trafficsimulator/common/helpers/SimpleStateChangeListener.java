496e8cca9d04db5f190b29cc9974f052376a2a10
Michal Szewczak <michaelszewczak@gmail.com>
2016-02-05 15:41:03 +0000

    add first version starter and simple state change listener that outputs text

--- src/main/java/thewaypointers/trafficsimulator/common/helpers/SimpleStateChangeListener.java
+++ src/main/java/thewaypointers/trafficsimulator/common/helpers/SimpleStateChangeListener.java
@@ -1,4 +1,18 @@
 package thewaypointers.trafficsimulator.common.helpers;
 
-public class SimpleStateChangeListener {
+import thewaypointers.trafficsimulator.common.IStateChangeListener;
+import thewaypointers.trafficsimulator.common.LocationDTO;
+import thewaypointers.trafficsimulator.common.WorldStateDTO;
+
+public class SimpleStateChangeListener implements IStateChangeListener{
+
+    @Override
+    public void NewStateReceived(WorldStateDTO state) {
+        LocationDTO loc = state.vehicles.get(0).location;
+        System.out.format("Vehicle 0 position: %f en route from %s to %s",
+                          loc.getDistanceTravelled(),
+                          loc.getRoad().start.label,
+                          loc.getRoad().end.label);
+        System.out.println();
+    }
 }
