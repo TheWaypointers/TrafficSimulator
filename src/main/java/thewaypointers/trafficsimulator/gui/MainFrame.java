8e96ba39325a7f82aaca562afb60983033ba9f6c
mengzhuwang <mengzhuw@outlook.com>
2016-02-17 08:56:28 +0000

    since the size of map exceed my screen, JScrollPanel() is used

--- src/main/java/thewaypointers/trafficsimulator/gui/MainFrame.java
+++ src/main/java/thewaypointers/trafficsimulator/gui/MainFrame.java
@@ -12,13 +12,18 @@ public class MainFrame extends JFrame{
     public MainFrame(WorldStateDTO worldStateDTO){
         mapPanel  =  new MapPanel();
         mapPanel.NewStateReceived(worldStateDTO);
-        this.setSize(800,600);
-        this.setVisible(true);
-        this.add(mapPanel);
+        mapPanel.setPreferredSize(new Dimension(1000,1000));
+        JScrollPane jScrollPane=new JScrollPane(mapPanel);
+        jScrollPane.setViewportView(mapPanel);
+
+        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        this.add(jScrollPane);
+        this.setSize(1200,1200);
+
         this.setTitle("traffic");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocation(200, 200);
-
+        this.setLocation(100, 0);
+        this.setVisible(true);
         //this.add(controlPanel);
 
     }
