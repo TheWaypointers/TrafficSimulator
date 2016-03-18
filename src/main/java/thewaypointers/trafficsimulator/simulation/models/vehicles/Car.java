e9c412f715d3dd1560500eecca7227a54c10356f
vezer93 <k3009965383326>
2016-03-18 23:19:42 +0000

    Revert "changed the mistakes"
    This reverts commit 824f7f00a97c3b2f17236e1431e6e0f553c1ae7a.

--- src/main/java/thewaypointers/trafficsimulator/simulation/models/vehicles/Car.java
+++ src/main/java/thewaypointers/trafficsimulator/simulation/models/vehicles/Car.java
@@ -77,7 +77,12 @@ public class Car implements IVehicle {
                     VehicleManager.removeVehicle(currentRoad, this);
                     RoadEdge nextRoadEdge = calculateNextRoad(nodeGraphMap);
 
-                    currentRoad = nextRoadEdge.getRoad();
+                    try{
+                        currentRoad = nextRoadEdge.getRoad();
+                    }
+                    catch (Exception ex){
+                        System.out.println("Lol");
+                    }
                     roadLength = nextRoadEdge.getRoadLength();
                     VehicleManager.getVehicleMap().get(currentRoad).add(this);
 
@@ -117,8 +122,8 @@ public class Car implements IVehicle {
             }
         }
 
-        for (RoadEdge road : nodeGraphMap.get(currentNode)) {
-            if (road.getOrigin().equals(currentNodeName) && road.getDestination().equals(nextNodeName)) {
+        for(RoadEdge road : nodeGraphMap.get(currentNode)){
+            if(road.getOrigin().equals(currentNodeName) && road.getDestination().equals(nextNodeName)){
                 return road;
             }
         }
