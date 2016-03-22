752c57fc568122083d39d97af9f9d9744f4bace6
Haipei Liu <liuhappy.nju@gmail.com>
2016-03-22 16:41:51 +0000

    Update Control Panel:    -add slider button for Simulation speed    -add slider button for States per second    -add checkbox for Debug    -change vehicles number to percentage    -display the signs for different type of vehicles    -make the buttons more recognized Implement pass parameters through Mainframe.SimulationInputListener

--- src/main/java/thewaypointers/trafficsimulator/common/SimulationInputListener.java
+++ src/main/java/thewaypointers/trafficsimulator/common/SimulationInputListener.java
@@ -3,6 +3,6 @@ package thewaypointers.trafficsimulator.common;
 public class SimulationInputListener implements ISimulationInputListener {
     @Override
     public void SimulationParameterChanged(String parameterName, String value) {
-
+        System.out.println(parameterName+": "+value);
     }
 }
