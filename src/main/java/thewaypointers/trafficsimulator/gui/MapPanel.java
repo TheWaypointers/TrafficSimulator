403757e079ea9bc22b9e13eeba68a03aac7247da
mengzhuwang <mengzhuw@outlook.com>
2016-03-31 09:53:09 +0100

    fix average total speed

--- src/main/java/thewaypointers/trafficsimulator/gui/MapPanel.java
+++ src/main/java/thewaypointers/trafficsimulator/gui/MapPanel.java
@@ -121,29 +121,37 @@ public class MapPanel extends JPanel implements IStateChangeListener{
             }
         }
 
-
         double cautionCarSpeed = 0;
         double normalCarSpeed = 0;
         double recklessCarSpeed = 0;
         double ambulanceSpeed =0;
         double totalSpeed = 0;
+        int carTypeNum = 0;
         if(cautionCarTotalNum!=0){
             cautionCarSpeed = cautionCarTotalSpeed/cautionCarTotalNum;
             totalSpeed = totalSpeed + cautionCarSpeed;
+            carTypeNum++;
         }
         if(normalCarTotalNum!=0){
             normalCarSpeed = normalCarTotalSpeed/normalCarTotalNum;
             totalSpeed = totalSpeed + normalCarSpeed;
+            carTypeNum++;
         }
         if(recklessCarTotalNum!=0){
             recklessCarSpeed = recklessCarTotalSpeed/recklessCarTotalNum;
             totalSpeed = totalSpeed + recklessCarSpeed;
+            carTypeNum++;
         }
         if(ambulanceTotalNum!=0){
             ambulanceSpeed = ambulanceTotalSpeed/ambulanceTotalNum;
             totalSpeed = totalSpeed + ambulanceSpeed;
+            carTypeNum++;
         }
 
+        if (carTypeNum > 0)
+            totalSpeed = totalSpeed/carTypeNum;
+        else
+            totalSpeed = 0;
         MainFrame.statisticsPanel.addRow(cautionCarSpeed,normalCarSpeed,recklessCarSpeed,ambulanceSpeed,totalSpeed);
     }
 
